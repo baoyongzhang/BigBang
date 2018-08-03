705cd854f6be671fcd06f6a6d311034c37f26b90
jaqxues <32979131+jaqxues@users.noreply.github.com>
2018-08-03 02:04:01 +0200

    Optimizations
    Make as less calls as possible in that method. It is called on every app start and can influence the performance of your phone!
    
    Have fun!

--- core/src/main/java/com/baoyz/bigbang/core/xposed/XposedBigBang.java
+++ core/src/main/java/com/baoyz/bigbang/core/xposed/XposedBigBang.java
@@ -20,6 +20,8 @@ public class XposedBigBang implements IXposedHookLoadPackage {
 
     private Processor processor;
 
+    /* The constructor is called every time an app is loaded (in background OR foreground or even at system start)
+    so we should not do ANYTHING that slows down, no matter how small it is, it could get very expensive. */
     public XposedBigBang() {
         processor = new Processor();
         processor.addProcess(new FilterProcess());
@@ -27,9 +29,25 @@ public class XposedBigBang implements IXposedHookLoadPackage {
         processor.addProcess(new CommonAppProcess());
     }
 
+    // Same thing here, try to do all checks in a first place, as fast as possible, so you don't execute too many methods on any app start
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
         XposedUtils.setXpoedEnable(loadPackageParam);
         processor.process(loadPackageParam);
+        
+        
+        /* I recommend this
+        switch (loadPackageParam.classLoader) {
+            case "com.example.something_you_want_to_hook":
+                // Note that I did not pass loadPackageParam as an argument, but only the classLoader, small optimization
+                new ExampleHooks(loadPackageParam.classLoader); // or add an "init" method if you don't want to execute it with a constructor call:
+                // new ExampleHooks(lpparam.classLoader).init();
+                break;
+            default:
+                // Return on any other app, nothing should be hooked, no other calls are made.
+                return;
+        }
+        Log.d("Successfully initialized Hooks for app \"" + loadPackageParam.packageName + "\");
+        */
     }
 }
